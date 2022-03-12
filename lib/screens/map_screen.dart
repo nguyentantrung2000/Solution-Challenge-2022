9766bcd68bac18a988b88aa1f0af7927e3ed1a39
unknown <trung.nt4996@sinhvien.hoasen.edu.vn>
2022-03-12 16:47:31 +0700

    current location

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
@@ -11,8 +13,18 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  Completer<GoogleMapController> _controller = Completer();
+  var locationMess = "";
+  void getCurrentLocation() async {
+    var position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    var latPosition = await Geolocator.getLastKnownPosition();
+    print(latPosition);
+    setState(() {
+      locationMess = "$position.latitude,$position.longtitude";
+    });
+  }
 
+  Completer<GoogleMapController> _controller = Completer();
   List markers = [
     {
       "id": "_kGooglePlexMaker",
@@ -20,26 +32,12 @@ class _MapPageState extends State<MapPage> {
       "lat": "11.766871",
       "long": "108.801220"
     },
-    {
-      "id": "_kGooglePlexMaker",
-      "title": "Google Plex",
-      "lat": "11.766871",
-      "long": "108.801220"
-    },
-    {
-      "id": "'xanh ne'",
-      "title": "This is blue",
-      "lat": "11.766877",
-      "long": "108.801200"
-    }
   ];
 
-  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-  
         mapType: MapType.terrain,
         markers: markers.map((s) {
           return Marker(
@@ -49,6 +47,7 @@ class _MapPageState extends State<MapPage> {
                 BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
             position: (LatLng(double.parse(s['lat']), double.parse(s['long']))),
             onTap: () {
+              print(locationMess);
             },
           );
         }).toSet(),
@@ -59,11 +58,6 @@ class _MapPageState extends State<MapPage> {
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
       ),
     );
   }
